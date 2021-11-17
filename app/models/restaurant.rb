e812cdad39bab7c685244ba2a3e67d5437b3bf41
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-17 11:27:12 +0100

    restaurant validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,13 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  after_destroy :destroy_reviews
+
+  private
+
+  def after_destroy
+    reviews.destroy_all
+  end
 end
